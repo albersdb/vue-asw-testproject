a80017de2b1dbf1930714a2f470dc42cb6fdbd18
David Albers <albersdb@yahoo.com>
2023-02-26 14:20:39 -0800

    Trying to hide source files

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  devtool: '',
+  productionSourceMap: false,
+  cssSourceMap: false,
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
