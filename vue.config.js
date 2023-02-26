35eeff4839a6bdc079c24ea112262f65580a007b
David Albers <albersdb@yahoo.com>
2023-02-26 14:29:06 -0800

    another vue config change

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: false,
-  cssSourceMap: false,
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
